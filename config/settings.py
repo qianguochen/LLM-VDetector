60f864cb0993dab937705e53bd8463e5e3ff8fbb
qianguo-chen <chenqianguo@ctbu.edu.cn>
2026-04-21 11:19:08 +0800

    Remove sensitive API information

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,8 @@
 from config.enums import DetectMode, VulnerabilityType
 # 模型参数
 MODEL_NAME = 'gpt-4o'
-API_KEY = "sk-Zq367BUJF7Jx70LG77GuWOFTd6MuUsqHtWMvh8ItwSXE8wtW"
-API_URL = "https://api.vectorengine.ai/v1/chat/completions"
+API_KEY = "******"
+API_URL = "******"
 
 # 检测模式
 DETECT_MODES = DetectMode.SOURCE_AND_CFG
